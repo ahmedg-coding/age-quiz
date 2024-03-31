1682fb924ed4bf1a082009bfec277fd75db10ca7
Ahmed G. <ahmedgithub@outlook.com>
2024-03-31 19:36:51 +0100

    Add constants for age thresholds in age-quiz.py

--- age-quiz.py
+++ age-quiz.py
@@ -1,14 +1,20 @@
+AGE_THRESHOLD_DEAD = 100
+AGE_THRESHOLD_RETIREMENT = 65
+AGE_THRESHOLD_OVER_HILL = 40
+AGE_THRESHOLD_KIDDIE_DISCOUNT = 13
+AGE_THRESHOLD_21 = 21
+
 age = int(input("Enter your age: "))
 
-if age >= 100:
+if age >= AGE_THRESHOLD_DEAD:
     print("Sorry, you're dead.")
-elif age >= 65:
+elif age >= AGE_THRESHOLD_RETIREMENT:
     print("Enjoy your retirement!")
-elif age >= 40:
+elif age >= AGE_THRESHOLD_OVER_HILL:
     print("You're over the hill.")
-elif age < 13:
+elif age < AGE_THRESHOLD_KIDDIE_DISCOUNT:
     print("You qualify for the kiddie discount.")
-elif age == 21:
+elif age == AGE_THRESHOLD_21:
     print("Congrats on your 21st!")
 else:
     print("Age is but a number.")
